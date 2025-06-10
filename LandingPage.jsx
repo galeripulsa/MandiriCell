cede3442aa6aa0b7aca2f4c36d6687f7c27923fe
galeripulsa <agusbaba1987@gmail.com>
2025-06-11 01:27:49 +0800

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { FiZap, FiUser, FiHome, FiMapPin, FiLock, FiGift } from "react-icons/fi";
+
+// ... (file content was already provided above)
+
+export default function LandingPage() {
+  // (the full content from the canvas is assumed to be here)
+}
